e901832d1161a8747503b3b137fb78dbb6802a11
melihabak <melihfurkanabak@gmail.com>
2023-10-16 14:26:41 +0300

    First Commit Added

---
+++ src/test/java/appiumtests/A0_UiAutomator2Options.java
@@ -0,0 +1,178 @@
+package appiumtests;
+
+import java.io.File;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+/*
+ * As per Java-Client 8
+ * Appium 2
+ * 
+ * adb device
+ * adb shell getprop ro.build.version.release
+ * adb shell dumpsys window windows | grep mFocusedApp
+ * 
+ */
+
+public class A0_UiAutomator2Options {	
+	
+	public UiAutomator2Options getAPIDemosAPKOptions(){
+		
+		System.out.println("----------Started: Api Demo Apk----------");
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("builds/ApiDemos-debug.apk").getFile());
+		String apkPath = file.getAbsolutePath();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("11.0")
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("emulator-5554")
+			
+			.setAppPackage("io.appium.android.apis")
+			.setAppActivity(".ApiDemos")
+			
+			.setApp(apkPath)
+			.setNoReset(false);
+		
+		return options;
+		
+	}
+	
+	public UiAutomator2Options getSauceLabApkOptions(){
+		
+		System.out.println("----------Started: Sauce Lab Demo Apk----------");
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("builds/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk").getFile());
+		String apkPath = file.getAbsolutePath();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("11.0")
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("emulator-5554")
+			
+			.setAppPackage("com.swaglabsmobileapp")
+			.setAppActivity(".MainActivity")
+			
+			.setApp(apkPath)
+			.setNoReset(false);
+		
+		return options;
+		
+	}
+	
+	public UiAutomator2Options getWebdriverIOApkOptions(){
+		
+		System.out.println("----------Started: WebdriverIO Demo Apk----------");
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("builds/Android-NativeDemoApp-0.4.0.apk").getFile());
+		String apkPath = file.getAbsolutePath();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("12.0")		
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("R68R9042DXV")
+			
+			.setAppPackage("com.wdiodemoapp")
+			.setAppActivity(".MainActivity")
+			
+			.setApp(apkPath)
+			.setNoReset(false);
+		
+		return options;
+		
+	}
+	
+	
+	public UiAutomator2Options getChromeOptions(){
+		
+		System.out.println("----------Started: Chrome Browser----------");
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("12.0")
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("R68R9042DXV")
+
+			.noReset()
+			
+			.withBrowserName("Chrome");
+			
+		return options;
+		
+	}
+	
+	public UiAutomator2Options getWebdriverIOApkOptions2(){
+		
+		System.out.println("----------Started: WebdriverIO Demo Apk----------");
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("builds/Android-NativeDemoApp-0.4.0.apk").getFile());
+		String apkPath = file.getAbsolutePath();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("12.0")
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("R68R9042DXV")
+			
+			.setAppPackage("com.wdiodemoapp")
+			.setAppActivity(".MainActivity")
+			
+			.setApp(apkPath)
+			.setNoReset(false);
+		
+		return options;
+		
+	}
+	
+	
+	public UiAutomator2Options smartpanelAppTest(){
+		
+		System.out.println("----------Started: Smartpanel Mobile Application----------");
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("builds/smarttoilet.apk").getFile());
+		String apkPath = file.getAbsolutePath();
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		options
+			.setPlatformName("Android")
+			.setPlatformVersion("12.0")		
+			
+			.setAutomationName("UiAutomator2")
+			.setDeviceName("R68R9042DXV")
+			
+			.setAppPackage("com.vcsp")
+			.setAppActivity("com.vitra.vitra_smart_panel_application.MainActivity")
+			
+			.setApp(apkPath)
+			.setNoReset(false);
+		
+		return options;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
---
+++ src/test/java/appiumtests/A1_AppiumBasicApp1.java
@@ -0,0 +1,108 @@
+package appiumtests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+
+public class A1_AppiumBasicApp1 {
+
+	static AndroidDriver driver;
+	
+	
+	// App > Activity > Animation > Back to Home Page
+	public void testBasicFlow() {
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"App\"]")).click();
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Activity\"]")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\'Animation\']")).click();
+		driver.navigate().back();
+		driver.navigate().back();
+		driver.navigate().back();
+		driver.navigate().back();
+
+	}
+	
+	
+	// Content > Assets > Read Asset > Copy Given Text > Back to Home Page
+	public void testXPath() {
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Content\"]")).click();
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Assets\"]")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Read Asset\"]")).click();
+		driver.navigate().back();
+		driver.navigate().back();
+		driver.navigate().back();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Preference\"]")).click();
+		driver.navigate().back();
+		
+	}
+	
+	public void testClassNameLocator() {
+		
+		System.out.println("----------Element Name----------");
+		List<WebElement> list = driver.findElements(By.className("android.widget.TextView"));
+		for(WebElement ele : list) {		
+			switch(ele.getText()) {
+			  case "API Demos":
+			    break;			
+			  default:
+				  System.out.println(ele.getText());
+			}
+		}	
+	}
+	
+	public void testIdAndAccessibilityId() {
+		
+		driver.findElement(AppiumBy.id("android:id/text1")).click();
+		driver.navigate().back();
+		driver.findElement(AppiumBy.accessibilityId("Animation")).click();
+		driver.navigate().back();
+		driver.findElement(AppiumBy.accessibilityId("App")).click();
+		driver.navigate().back();
+		
+	}
+	
+	public void testGetAttribute() {
+		
+		String name = driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\'Accessibility\']"))
+				.getAttribute("name");
+		System.out.println(name);
+		
+		String getText = driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Accessibility']"))
+				.getText();
+		System.out.println(getText);
+		
+		String contentDesc = driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Accessibility']"))
+				.getAttribute("content-desc");
+		System.out.println(contentDesc);
+		   
+		String bounds = driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Accessibility']"))
+				.getAttribute("bounds");
+		System.out.println(bounds);
+		
+	}
+	
+	public static void main(String[] args) throws MalformedURLException {
+
+
+		A0_UiAutomator2Options options = new A0_UiAutomator2Options();
+		
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),
+				options.getAPIDemosAPKOptions());
+		
+		A1_AppiumBasicApp1 obj = new A1_AppiumBasicApp1 ();
+		//obj.testBasicFlow();
+		//obj.testXPath();
+		//obj.testClassNameLocator();
+		//obj.testIdAndAccessibilityId();
+		obj.testGetAttribute();
+		System.out.println("----------Run Finished----------");
+	}
+
+}
